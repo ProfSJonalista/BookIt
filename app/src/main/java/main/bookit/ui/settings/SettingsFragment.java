dbff0db4293e73e40d73b2897959aa9c9efdaa7e
Jan Miętki <jan.mietki@gmail.com>
2018-12-16 14:38:29 +0100

    Added initial two language support, settings activity and fragment

---
+++ app/src/main/java/main/bookit/ui/settings/SettingsFragment.java
@@ -0,0 +1,49 @@
+package main.bookit.ui.settings;
+
+import android.arch.lifecycle.ViewModelProviders;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import main.bookit.R;
+
+public class SettingsFragment extends Fragment {
+
+    private SettingsViewModel mViewModel;
+    TextView text;
+    Button saveButton;
+
+    public static SettingsFragment newInstance() {
+        return new SettingsFragment();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.settings_fragment, container, false);
+        Spinner spinner = (Spinner) view.findViewById(R.id.languageSpinner);
+        String[] values = { "English", "Polski"};
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this.getActivity(), android.R.layout.simple_spinner_item, values);
+        spinner.setAdapter(adapter);
+
+        return view;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = ViewModelProviders.of(this).get(SettingsViewModel.class);
+        // TODO: Use the ViewModel\
+    }
+}
